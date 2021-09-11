a30b66dd0b7e462017e7d758ff55f8c5b87447a3
Tomilov Sergey <tomexpert@yandex.ru>
2021-09-11 11:06:54 +0800

    webpack it`s work

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -23,5 +24,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'style.css',
     }),
+    new CleanWebpackPlugin(),
   ],
 };
